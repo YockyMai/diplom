6251d7cc01305d9993cff9996390a0832c296e04
YockyMai <g.valera15102003@gmail.com>
2022-12-14 05:50:20 +0500

    Добавил инфу для чего нужна регистрация

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,24 +3,50 @@ import {
 	Container,
 	Title,
 	Text,
-	Stack,
+	Stack, List, Center, Button, Alert,
 } from '@mantine/core';
-import { CatalogLinkCards } from '../components/CatalogLinkCards';
+import {CatalogLinkCards} from '../components/CatalogLinkCards';
+import {CirclePlus, UserCircle} from "tabler-icons-react";
+import {Link} from "react-router-dom";
 
 export const Main = () => {
 
-	return (
-		<Container style={{ textAlign: 'center' }} mt={100} size={'xl'}>
-			<Stack>
-				<Title order={2} align={'center'}>
-					Добро пожаловать, мы - Sneakers always, онлайн магазин
-					который готов предоставить тебе качественный товар!
-				</Title>
-				<Text size="lg" mb={50} color={'dimmed'}>
-					Выбирай свою категорию и бегом за покупками!
-				</Text>
-				<CatalogLinkCards />
-			</Stack>
-		</Container>
-	);
+  return (
+    <Container mt={100} size={'xl'}>
+      <Stack style={{textAlign: 'center'}}>
+        <Title order={2} align={'center'}>
+          Добро пожаловать, мы - Sneakers always, онлайн магазин
+          который готов предоставить тебе качественный товар!
+        </Title>
+        <Text size="lg" mb={50} color={'dimmed'}>
+          Выбирай свою категорию и бегом за покупками!
+        </Text>
+        <CatalogLinkCards/>
+      </Stack>
+			<Container mt={80} size={"sm"}>
+				<Alert title={
+					<Text align={"center"} weight={900} >Зарегистрируйтесь на нашем сайти и получите следующие преимущества:</Text>
+				}>
+					<Stack align={"left"} mt="xl">
+						<Center>
+							<List spacing={4}>
+								<List.Item icon={<CirclePlus color={"#228DE9"}/>}>
+									Доступ к премиум товарам высшего качества.
+								</List.Item>
+								<List.Item icon={<CirclePlus color={"#228DE9"}/>}>
+									Возможность добавлять товары в корзину и совершать покупки.
+								</List.Item>
+								<List.Item icon={<CirclePlus color={"#228DE9"}/>}>
+									Возможность комментировать и оставлять отзывы о товарах.
+								</List.Item>
+							</List>
+						</Center>
+						<Button mb={"sm"} leftIcon={<UserCircle/>} component={Link} to={"/auth/signup"} m={"auto"}>
+							Зарегистрироваться
+						</Button>
+					</Stack>
+				</Alert>
+			</Container>
+    </Container>
+  );
 };
